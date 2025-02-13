f16967cedb6b4b3ffa71464e6622c577b12d19de
oOXxTNTxXOo <bob.bross10@gmail.com>
2025-02-13 10:24:09 -0700

    broken

--- scan2.py
+++ scan2.py
@@ -10,45 +10,65 @@ import time
 import math
 from scapy.all import *
 from ipaddress import ip_network
+import threading
 
 a = sys.argv[1] # scanning method 
-# b = sys.argv[2] # ports to scan 
+b = sys.argv[2] # ports to scan 
 # c = sys.argv[3] # specifies ip address range
 
 def scan_net():
     total_start_time = time.time()
-    ip_range = ip_network(a, strict=False)
+    ip_range = ip_network(b, strict=False)
+    scanningM = a
     print(f"IP range: {ip_range}")
-    
-    for ip in ip_range.hosts():
+
+    for ip in ip_range.hosts(): # scans the range of specifed ips
         responsTimestart = time.time()
         status = 'N/A'
         toScan = f"{ip}"
-        if ping(toScan):
-            status = "UP"
-            print(f"{ip}  ({status}) ({(math.floor((time.time() - responsTimestart)*100))/100}ms)")
-            open_ports(toScan)
-        else:
-            status = "DOWN"
-            print(f"{ip}  ({status}) ({(math.floor((time.time() - responsTimestart)*100))/100}ms)")
+        ipstatus(responsTimestart, status, toScan, ip)
     print(f"total scan time: {math.floor(((time.time()) - total_start_time) * 1000)}ms")     
 
-def ping(host):
-    packet = IP(dst=host)/ICMP()
-    response = sr1(packet, timeout=2, verbose=0)
-    if response is None:
-        return False
-    elif response.haslayer(ICMP):
-        return True
+def ipstatus(responsTimestart, status, toScan, ip):
+    specific_ports = str(a)
+    if ping(toScan): # checks if ip is up or down
+        status = "UP"
+        print(f"{ip}  ({status}) ({(math.floor((time.time() - responsTimestart)*100))/100}ms)")
+        open_ports(toScan, specific_ports)
+    else:
+        status = "DOWN"
+        print(f"{ip}  ({status}) ({(math.floor((time.time() - responsTimestart)*100))/100}ms)")
 
-def open_ports(ip):
+def ping(host): # sends a packet to specified ip
+    packet = IP(dst=host)/ICMP()
+    try: 
+        response = sr1(packet, timeout=2, verbose=0)
+        if response is None: # verifies if packet was sent & recieved
+            return False
+        elif response.haslayer(ICMP):
+            return True
+    except Exception as e:
+        print(f"Error scanning {host}")
+        return False
+
+def open_ports(ip, ports=None): # checks all ports
+    if ports is None:
+        return print("enter valid port range")
+    elif ports == "A":
+        ports = [21, 80, 443] #defualt port range
+    else:
+        ports = ports
+    
     start = 1
     end = 1024
     for port in range(start, end):
         pkt = IP(dst=ip) / TCP(dport=port, flags="S")
-        response = sr1(pkt, timeout=1, verbose=0)
-        if response is not None and response.haslayer(TCP):
-            if response[TCP].flags == "SA":  # SYN-ACK response
-                print(f"    - Port {port} (OPEN)")
+        try:
+            response = sr1(pkt, timeout=1, verbose=0)
+            if response is not None and response.haslayer(TCP):
+                if response[TCP].flags == "SA":  # SYN-ACK response
+                    print(f"    - Port {port} (OPEN)")
+        except Exception as e:
+            print(f"Error scanning {port}")
 
 scan_net()
